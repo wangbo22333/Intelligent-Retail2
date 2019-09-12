3f27dda320a204f6ba8a48ebfa9302979826e2b1
bo wang <wang5323@outlook.com>
2019-09-12 11:43:11 +0800

    修改基础页面

---
+++ Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelligent_Retail2.Models
+{
+    public class UserModel
+    {
+        
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }    
+        public string UserNumber { get; set; }  
+        public string UserPhone { get; set; }
+        public string UserEmail { get; set; }
+
+
+    }
+}
