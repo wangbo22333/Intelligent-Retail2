46a06e6a969da32c8dc40d7fe627f6344f490e6f
bo wang <wang5323@outlook.com>
2019-09-12 16:06:05 +0800

    修改用户模板

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Intelligent_Retail2.Models
 {
-    public class UserModel
+    public class User
     {
         
         public int ID { get; set; }
-        public string UserName { get; set; }
-        public string PassWord { get; set; }    
-        public string UserNumber { get; set; }  
+        public string UserNickName { get; set; }
+        //public string PassWord { get; set; }    
+        public string UserOpenID { get; set; }  
         public string UserPhone { get; set; }
-        public string UserEmail { get; set; }
-
+        public string UserGender { get; set; }
+        
 
     }
 }
